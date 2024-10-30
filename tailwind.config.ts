2d102510a8ef6969bdf91f4a58708a54b068c1fd
Muhmmad-Sufyan <144341878+Muhmmad-Sufyan@users.noreply.github.com>
2024-10-30 17:13:11 +0500

    Add files via upload

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,12 +11,41 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
-        
       },
-      
+      fontFamily: {
+        chocolates: ["TT Chocolates Trl", "sans-serif"],
+      },
+      container: {
+        center: true,
+        padding: "1rem",
+        screens: {
+          sm: "640px",
+          md: "768px",
+          lg: "1024px",
+          xl: "1280px",
+          // 2xl is intentionally omitted here
+        },
+      },
     },
   },
   plugins: [],
 };
 export default config;
 
+// module.exports = {
+//   theme: {
+//     extend: {},
+//     container: {
+//       center: true,
+//       padding: '1rem',
+//       screens: {
+//         sm: '640px',
+//         md: '768px',
+//         lg: '1024px',
+//         xl: '1280px',
+//         // 2xl is intentionally omitted here
+//       },
+//     },
+//   },
+//   plugins: [],
+// };
